095b400c2ff35756f6f25b349897c6138ce06310
Almeonamy <almeonamy@gmail.com>
2020-07-07 15:08:25 +0700

    Add project files.

---
+++ FrontTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FrontTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
